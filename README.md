d2b552576fa2937448d3ab0c8e4adc8f1929f2cc
ceciaups <cecilia127@msn.com>
2023-04-07 00:51:18 -0400

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Movie Night
+- Couple or friends can decide the movie for tonight!
+- By choosing two movies, 10 results will be generated
+- Implemented two APIs: TMDB and OpenAI
+![Movie Night page](https://raw.githubusercontent.com/ceciaups/Movie_Night/master/public/movie_night.png)
+
+## Features
+- Movies search engine
+- Results by OpenAI
+
+## Links
+- Views: [/views/index.pug](views/index.pug)
+- CSS: [/public/styles.css](public/styles.css)
+- JS: [/public/script.js](public/script.js)
+- Node.js: [/index.js](index.js)
+- API modules: [/modules](/modules)
 
--- modules/chatgpt/api.js
+++ modules/chatgpt/api.js
@@ -12,12 +12,15 @@ async function chatCompletion(title1, title2) {
   let messages = [];
   messages.push({ role: "user", content: `${strTitle1} and ${strTitle2}, give me 10 similar movies.` });
 
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: messages,
-  });
+  console.log(messages[0].content);
+  return "1. Avatar 2. The Shape of Water 3. Elysium 4. Oblivion 5. Tron: Legacy 6. Inception 7. Total Recall 8. Blade Runner 2049 9. Interstellar 10. The Abyss.";
 
-  return completion.data.choices[0].message.content;
+  // const completion = await openai.createChatCompletion({
+  //   model: "gpt-3.5-turbo",
+  //   messages: messages,
+  // });
+
+  // return completion.data.choices[0].message.content;
 }
 
 module.exports = {
