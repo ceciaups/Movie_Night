58c93bc52fb6b171b34b3aea5a588c1c13eb7e8d
ceciaups <cecilia127@msn.com>
2023-04-07 00:57:05 -0400

    Update OpenAI API

--- modules/chatgpt/api.js
+++ modules/chatgpt/api.js
@@ -12,15 +12,12 @@ async function chatCompletion(title1, title2) {
   let messages = [];
   messages.push({ role: "user", content: `${strTitle1} and ${strTitle2}, give me 10 similar movies.` });
 
-  console.log(messages[0].content);
-  return "1. Avatar 2. The Shape of Water 3. Elysium 4. Oblivion 5. Tron: Legacy 6. Inception 7. Total Recall 8. Blade Runner 2049 9. Interstellar 10. The Abyss.";
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: messages,
+  });
 
-  // const completion = await openai.createChatCompletion({
-  //   model: "gpt-3.5-turbo",
-  //   messages: messages,
-  // });
-
-  // return completion.data.choices[0].message.content;
+  return completion.data.choices[0].message.content;
 }
 
 module.exports = {
